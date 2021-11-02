ee5dcb40baa8ef917c72c88149ac6ed884a5cf0e
HP Omen <pochtaalberta@mail.ru>
2021-11-02 12:01:52 +0500

    Final

--- lesson12task4.2/main.cpp
+++ lesson12task4.2/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numbers[15] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
+    int x = numbers[0];
+    int result = 0;
+    for(int i = 0; i < 15; i++){
+        if (numbers[i] < x){
+            x = numbers[i];
+        }
+    }
+    for (int j = 0; j < 15; ++j) {
+        int counter = 0;
+        for(int i = 0; i < 15; i++){
+            if ((x + j) == numbers[i]){
+                counter++;
+                if(counter == 2){
+                    result = x + j;
+                    break;
+                }
+            }
+        }
+    }
+    std::cout << "Duplicate " << result << std::endl;
+    std::cout << "X " << x << std::endl;
     return 0;
 }
