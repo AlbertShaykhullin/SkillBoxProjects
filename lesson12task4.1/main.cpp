f642024731a377bf350b972a7e8a92fd177a8385
HP Omen <pochtaalberta@mail.ru>
2021-11-02 10:41:13 +0500

    Realized cycle

--- lesson12task4.1/main.cpp
+++ lesson12task4.1/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::string house[10];
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string house[2];
+    for (int i = 0; i < 2; i++){
+        std::cin >> house[i];
+    }
+    for (int i = 0; i < 2; i++){
+        std::cout << house[i] << std::endl;
+    }
 }
