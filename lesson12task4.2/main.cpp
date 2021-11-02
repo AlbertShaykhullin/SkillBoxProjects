5facb1ee046150f891934156f638a86c1d4074c2
HP Omen <pochtaalberta@mail.ru>
2021-11-02 15:19:38 +0500

    Final corrected

--- lesson12task4.2/main.cpp
+++ lesson12task4.2/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    int numbers[15] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
+    int numbers[15] = {114, 111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
+    int numbers2[15] = {0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0};
     int x = numbers[0];
     int result = 0;
     for(int i = 0; i < 15; i++){
@@ -9,19 +10,16 @@ int main() {
             x = numbers[i];
         }
     }
-    for (int j = 0; j < 15; ++j) {
-        int counter = 0;
-        for(int i = 0; i < 15; i++){
-            if ((x + j) == numbers[i]){
-                counter++;
-                if(counter == 2){
-                    result = x + j;
-                    break;
-                }
-            }
+
+    for(int i = 0; i < 15; i++){
+        if(numbers2[numbers[i] - x] == 0){
+            numbers2[numbers[i] - x] = numbers[i] - x;
+        } else {
+            result = i;
         }
     }
-    std::cout << "Duplicate " << result << std::endl;
+
+    std::cout << "Duplicate " << numbers[result] << std::endl;
     std::cout << "X " << x << std::endl;
     return 0;
 }
