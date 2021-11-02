bb7e32836c458cfa89b9d9e4d1424226fb47db5d
HP Omen <pochtaalberta@mail.ru>
2021-11-02 11:23:01 +0500

    Final

--- lesson12task4.1/main.cpp
+++ lesson12task4.1/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::string house[2];
-    for (int i = 0; i < 2; i++){
+    int apartmentNumber[] = {0, 0, 0};
+    std::string house[10];
+    for (int i = 0; i < 10; i++){
         std::cin >> house[i];
     }
-    for (int i = 0; i < 2; i++){
-        std::cout << house[i] << std::endl;
+    for (int i = 0; i < 3; i++){
+        std::cin >> apartmentNumber[i];
+        if (apartmentNumber[i] > 10 || apartmentNumber[i] < 1) {
+            std::cout << "Error! wrong number of apartment" << std::endl;
+        } else {
+            std::cout << house[apartmentNumber[i]-1] << std::endl;
+        }
     }
 }
