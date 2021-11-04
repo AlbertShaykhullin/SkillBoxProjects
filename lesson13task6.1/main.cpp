062396a7f33e869c39f123041bf38c7d9c38d9f9
HP Omen <pochtaalberta@mail.ru>
2021-11-04 17:28:29 +0500

    Initial

---
+++ lesson13task6.1/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> erase_element (std::vector<int> vec, int deleted){
+    for (int i = 0; i < vec.size(); ++i) {
+        while(vec[i] == deleted && i < vec.size()){
+            vec.erase(vec.cbegin() + i);
+        }
+    }
+    return vec;
+}
+
+void print_vector(std::vector<int> vec){
+    for (int i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int count = 0;
+    int input = 0;
+    int deleted = 0;
+    std::cout << "Enter vector size:";
+    std::vector<int> vec;
+    std::cin >> count;
+    for (int i = 0; i < count; ++i) {
+        std::cout << "Enter vector number:";
+        std::cin >> input;
+        vec.push_back(input);
+    }
+    print_vector(vec);
+    std::cout << "Enter number to delete:";
+    std::cin >> deleted;
+    erase_element(vec, deleted);
+    print_vector(vec);
+    return 0;
+}
