7e2e3bf25585569379e4360549b348916ad0d175
HP Omen <pochtaalberta@mail.ru>
2021-11-04 20:20:52 +0500

    Corrected purchase

--- lesson13task6.2/main.cpp
+++ lesson13task6.2/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 
-std::vector<float> full_price (std::vector<float> price, std::vector<int> buy){
+std::vector<float> full_price (std::vector<float> price, std::vector<int> purchase){
     std::vector<float> result;
-    for (int i = 0; i < buy.size(); ++i){
-        result.push_back(price[buy[i]]);
+    for (int i = 0; i < purchase.size(); ++i){
+        result.push_back(price[purchase[i]]);
     }
     return result;
 }
@@ -17,17 +17,26 @@ float sum_elements(std::vector<float> vec){
     return sum;
 }
 
-void print_vector(std::vector<float> vec){
+void print_vector_float(std::vector<float> vec){
+    for (int i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+}
+void print_vector_int(std::vector<int> vec){
     for (int i = 0; i < vec.size(); ++i) {
         std::cout << vec[i] << " ";
     }
     std::cout << std::endl;
 }
 
+
 int main() {
     std::vector<float> price = {2.5, 4.25, 3.0, 10.0};
-    std::vector<int> buy = {1, 1, 0, 3};
-    std::vector<float> result = full_price(price, buy);
-    print_vector(result);
+    std::vector<int> purchase = {1, 1, 0, 3};
+    std::vector<float> result = full_price(price, purchase);
+    print_vector_float(price);
+    print_vector_int(purchase);
+    print_vector_float(result);
     std::cout << "Summary:" << sum_elements(result);
 }
