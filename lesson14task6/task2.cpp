80f2dfadc40848cba3e3cd817232877da7b8cf49
HP Omen <pochtaalberta@mail.ru>
2021-11-09 22:54:13 +0500

    Corrected

--- lesson14task6/task2.cpp
+++ lesson14task6/task2.cpp
@@ -27,9 +27,9 @@ void print_field(char gameField[3][3]){
 }
 
 std::string winner (char gameField[3][3], char type){
-    int winVertical = 0;
-    int winHorizontal = 0;
     for (int i = 0; i < 3; ++i) {
+        int winVertical = 0;
+        int winHorizontal = 0;
         for (int j = 0; j < 3; ++j) {
             if(gameField[i][j] == type){
                 winVertical++;
@@ -46,28 +46,39 @@ std::string winner (char gameField[3][3], char type){
     return "No winners";
 }
 
-void game_part(char gameField[3][3], char type, bool &gameRun){
-    std::string gameStatus;
-    enter_xy(gameField, type);
-    gameStatus = winner(gameField,type);
-    if (gameStatus != "No winners"){
-        gameRun = false;
+void game_part(char gameField[3][3], char type, bool &gameRun, int &counter){
+    if (counter <= 9){
+        std::string gameStatus;
+        enter_xy(gameField, type);
+        counter++;
+        gameStatus = winner(gameField,type);
+        if (gameStatus == "No winners" && counter == 9){
+            gameRun = false;
+            print_field(gameField);
+            std::cout << "Draw - there is no winners"<< std::endl;
+        } else if(gameStatus != "No winners"){
+            gameRun = false;
+            print_field(gameField);
+            std::cout << "Step "<< type << ":" << gameStatus<< std::endl;
+        } else {
+            print_field(gameField);
+            std::cout << "Step "<< type << ":" << gameStatus<< std::endl;
+        }
     }
-    print_field(gameField);
-    std::cout << "Step "<< type << ":" << gameStatus<< std::endl;
 }
 
 int main() {
     std::string gameStatus;
     bool gameRun = true;
+    int counter = 0;
     char gameField[3][3] = {{'-', '-', '-'},
                             {'-', '-', '-'},
                             {'-', '-', '-'}};
     print_field(gameField);
     for (int i = 0; i < 9; ++i) {
-        game_part(gameField, 'X', gameRun);
+        game_part(gameField, 'X', gameRun, counter);
         if (!gameRun) break;
-        game_part(gameField, 'O', gameRun);
+        game_part(gameField, 'O', gameRun,counter);
         if (!gameRun) break;
     }
 }
