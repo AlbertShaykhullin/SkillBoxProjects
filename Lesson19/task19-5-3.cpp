8fc46ccb2d7e8cf2d34d53931c4cb15c34f1c584
HP Omen <pochtaalberta@mail.ru>
2022-05-14 20:51:51 +0500

    fix

--- Lesson19/task19-5-3.cpp
+++ Lesson19/task19-5-3.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-int sum_vec(std::vector<int> vector){
+/*int sum_vec(std::vector<int> vector){
     int summ = 0;
     for (int i = 0; i < vector.size(); ++i) {
         summ += vector[i];
@@ -27,38 +27,47 @@ int max_index_vec(std::vector<int> vector){
             return i;
         }
     }
-}
+}*/
 
 int main() {
     std::ifstream bank;
     std::string path;
     std::string name;
     std::string surname;
-    std::vector<std::string> nameVector;
-    std::vector<std::string> surnameVector;
-    std::vector<int> moneyVector;
-    std::vector<std::string> dateVector;
+    std::string date;
+
+    std::string max_money_name;
+    std::string max_money_surname;
+    std::string max_money_date;
+
 
     int money;
-    std::string date;
+    int summ = 0;
+    int max_money = 0;
+
     std::cout << "Enter path of file " << std::endl;
     std::cin >> path;//"E:\table.txt"
     bank.open(path);
     //buffer[20] = '\0';
+
     if (bank.is_open()){
         while(!bank.eof()){
             bank >> name >> surname >> money >> date;
-            nameVector.push_back(name);
-            surnameVector.push_back(surname);
-            moneyVector.push_back(money);
-            dateVector.push_back(date);
+            if (money > max_money){
+                max_money = money;
+                max_money_name = name;
+                max_money_surname = surname;
+                max_money_date = date;
+            }
+            summ += money;
+
             std::cout << name << " " << surname << " " << money << " " << date << std::endl;;
         }
     }
-    std::cout << "Total money payment: " << sum_vec(moneyVector) << std::endl;
-    std::cout << "Max money payment: " << max_vec(moneyVector) << std::endl;
-    std::cout << "Max money receiver: " << nameVector[max_index_vec(moneyVector)];
-    std::cout << " " << surnameVector[max_index_vec(moneyVector)] << std::endl;
+    std::cout << "Total money payment: " << summ << std::endl;
+    std::cout << "Max money payment: " << max_money << std::endl;
+    std::cout << "Max money receiver: " << max_money_name;
+    std::cout << " " << max_money_surname << std::endl;
     bank.close();
     return 0;
 }
